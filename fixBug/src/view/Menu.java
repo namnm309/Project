4cc8e8f1d702233980b6d8a44a22b08e09591c68
namnm309 <137751495+namminhs394@users.noreply.github.com>
2024-11-19 15:23:37 +0700

    chinhsuaview

--- fixBug/src/view/Menu.java
+++ fixBug/src/view/Menu.java
@@ -34,7 +34,7 @@ public class Menu {
         mainMenu.add("Displaying a hotel list ");
         mainMenu.add("Exit");
         while (true) {
-            System.out.println("=========== Menu ============");
+            System.out.println("=========== Menu Ohayo ============");
             int choice = getIntChoice(mainMenu);
             switch (choice) {
                 case 1:
