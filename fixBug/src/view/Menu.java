977aa11a06fe0fd7849e6bbf8d5bea3f6fb6184e
Vaio <namminhnam306@gmail.com>
2024-11-19 22:54:37 +0700

    capnhatlan3

--- fixBug/src/view/Menu.java
+++ fixBug/src/view/Menu.java
@@ -34,7 +34,7 @@ public class Menu {
         mainMenu.add("Displaying a hotel list ");
         mainMenu.add("Exit");
         while (true) {
-            System.out.println("=========== Menu Ohayo Adudu 333  55555============");
+            System.out.println("=========== Menu Ohayo Adudu 333  55555 99999 ============");
             int choice = getIntChoice(mainMenu);
             switch (choice) {
                 case 1:
